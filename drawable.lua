bfe4dbc7e82a04ab31b11f7306cf04369f322678
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2015-08-11 22:15:39 -0700

    make update, drawable default update/draw functions call cont()

--- drawable.lua
+++ drawable.lua
@@ -4,7 +4,7 @@ entity.create_named('drawable')
 
 entities.drawable.drawing = false -- whether to receive draw events
 
-function entities.drawable.draw(self, cont) end
+function entities.drawable.draw(self, cont) cont() end
 
 function entities.drawable.draw_rsubs(self, cont)
   for e in pairs(self:rsub_ids()) do
--- update.lua
+++ update.lua
@@ -4,7 +4,7 @@ entity.create_named('update')
 
 entities.update.updating = false -- whether to receive update events
 
-function entities.update.update(self, cont, dt) end
+function entities.update.update(self, cont, dt) cont(dt) end
 
 function entities.update.update_rsubs(self, cont, dt)
   for e in pairs(self:rsub_ids()) do
