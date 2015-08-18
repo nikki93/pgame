3c9d3b569923d7a5a987b618fa4fe0b81c59a025
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2015-08-18 02:48:52 -0700

    add entities.update.skip_next_frame

--- update.lua
+++ update.lua
@@ -3,10 +3,15 @@
 entity.create_named('update')
 
 entities.update.updating = false -- whether to receive update events
+entities.update.skip_next_frame = false -- to avoid dt spikes on process pause
 
 function entities.update.update(self, cont, dt) cont(dt) end
 
 function entities.update.update_rsubs(self, cont, dt)
+  if self.skip_next_frame then
+    self.skip_next_frame = false
+    return
+  end
   for e in pairs(self:rsub_ids()) do
     local e = entities[e]
     if e.updating then e:update(dt) end
