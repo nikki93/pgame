38a145f66a98fae43a52bafe230879f390f39887
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2015-08-20 03:29:10 -0700

    fix indentation in love.run()

--- main.lua
+++ main.lua
@@ -59,59 +59,59 @@ local function safe_call(f)
 end
 
 function love.run()
-	if love.math then
-		love.math.setRandomSeed(os.time())
-		for i=1,3 do love.math.random() end
-	end
- 
-	if love.event then love.event.pump() end
- 
-	if love.load then love.load(arg) end
- 
-	-- we don't want the first frame's dt to include time taken by love.load
-	if love.timer then love.timer.step() end
- 
-	local dt = 0
- 
-	-- main loop
-	while true do
-		-- process events
-		if love.event then
-			love.event.pump()
-			for e,a,b,c,d in love.event.poll() do
-				if e == "quit" then
-					if not love.quit or not love.quit() then
-						if love.audio then
-							love.audio.stop()
-						end
-						return
-					end
-				end
+  if love.math then
+    love.math.setRandomSeed(os.time())
+    for i=1,3 do love.math.random() end
+  end
+
+  if love.event then love.event.pump() end
+
+  if love.load then love.load(arg) end
+
+  -- we don't want the first frame's dt to include time taken by love.load
+  if love.timer then love.timer.step() end
+
+  local dt = 0
+
+  -- main loop
+  while true do
+    -- process events
+    if love.event then
+      love.event.pump()
+      for e,a,b,c,d in love.event.poll() do
+        if e == "quit" then
+          if not love.quit or not love.quit() then
+            if love.audio then
+              love.audio.stop()
+            end
+            return
+          end
+        end
         safe_call(function () love.handlers[e](a,b,c,d) end)
       end
-		end
- 
-		-- calculate dt
-		if love.timer then
-			love.timer.step()
-			dt = love.timer.getDelta()
-		end
- 
-		-- update, draw
-		if love.update then
+    end
+
+    -- calculate dt
+    if love.timer then
+      love.timer.step()
+      dt = love.timer.getDelta()
+    end
+
+    -- update, draw
+    if love.update then
       safe_call(function () love.update(dt) end)
     end -- 0 if love.timer is disabled
- 
-		if love.window and love.graphics and love.window.isCreated() then
-			love.graphics.clear()
-			love.graphics.origin()
+
+    if love.window and love.graphics and love.window.isCreated() then
+      love.graphics.clear()
+      love.graphics.origin()
       if love.draw then
         safe_call(love.draw)
       end
-			love.graphics.present()
-		end
- 
-		if love.timer then love.timer.sleep(0.001) end
-	end
+      love.graphics.present()
+    end
+
+    if love.timer then love.timer.sleep(0.001) end
+  end
 end
 
