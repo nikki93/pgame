31bb6781d979c56b6109e029c0745799d99ddb70
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2015-08-20 04:25:52 -0700

    add options for love-release in conf.lua

--- conf.lua
+++ conf.lua
@@ -1,4 +1,29 @@
 function love.conf(t)
+  -- options for love-release
+  t.identity = nil
+  t.version = "0.9.2"
+  t.game_version = nil
+  t.icon = nil
+  t.console = false
+
+  t.title = "pgame"
+  t.author = "unknown"
+  t.email = nil
+  t.url = nil
+  t.description = nil
+
+  t.os = {
+    "love",
+    windows = {
+      x32       = true,
+      x64       = true,
+      installer = false,
+      appid     = nil,
+    },
+    "osx"
+  }
+
+  -- options for love
   t.window.title = "pgame"
   t.window.width = 800
   t.window.height = 600
