46699d76626e1ced89ff981a33553198baff9a92
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2015-09-08 23:12:46 -0700

    'next doc' is really actually 'last doc'

--- lib/doc.lua
+++ lib/doc.lua
@@ -1,13 +1,13 @@
 doc = {}
 
-doc._next_doc = nil
+doc._last_doc = nil
 
 -- returns a 'doc object' d, which contains the documentation string, and is
 -- initially 'unclaimed' (not attached to any entity/slot/method) -- d:claim()
 -- claims it and returns the documentation string, or nil if already claimed --
 -- can be used to prevent accidentally assigning the same doc to multiple things
 function DOC(str)
-  -- save doc and return
+  -- create doc object, save it as last doc, return
   local d = {
     claimed = false,
     str = str,
@@ -17,7 +17,7 @@ function DOC(str)
       return str
     end
   }
-  doc._next_doc = d
+  doc._last_doc = d
   return d
 end
 
@@ -25,8 +25,8 @@ end
 -- there is no direct reference to the related documentation object, such as
 -- when defining methods using 'function name(...) ... end' syntax
 function doc.pop_doc()
-  if not doc._next_doc then return nil end
-  local str = doc._next_doc:claim()
-  doc._next_doc = nil
+  if not doc._last_doc then return nil end
+  local str = doc._last_doc:claim()
+  doc._last_doc = nil
   return str
 end
